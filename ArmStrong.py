8ecb6a30a591edc4e0d1c31a4f9124afb66e2447
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-07-30 09:26:33 -0700

    Create ArmStrong.py

---
+++ ArmStrong.py
@@ -0,0 +1,12 @@
+number = 371
+num = number
+digit, sum = 0, 0
+length = len(str(num))
+for i in range(length):
+  digit = int(num%10)
+  num = num/10
+  sum += pow(digit,length)
+if sum==number:
+  print("Armstrong")
+else:
+  print("Not Armstrong")
