3efbc0dea94c1d283ee5b8fed8fd6d16441c82e4
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-07-31 09:52:20 -0700

    Create Fact.py

---
+++ Fact.py
@@ -0,0 +1,12 @@
+num = 6
+fact = 1
+
+# Factorial of negative number doesn't exist
+
+if num < 0:
+    print("Not Possible")
+else:
+    for i in range(1, num+1):
+        fact = fact * i
+
+print("Factorial of", num, "is", fact)
