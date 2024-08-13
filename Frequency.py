4fed0efea47e8adcec6ce68b89216ef2a66e0aa0
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-08-13 09:37:05 -0700

    Create Frequency.py

---
+++ Frequency.py
@@ -0,0 +1,27 @@
+def countFrequency(arr, size):
+
+    for i in range(0, size):
+        flag = False
+        count = 0
+
+        for j in range(i+1, size):
+            if arr[i] == arr[j]:
+                flag = True
+                break
+
+        # The continue keyword is used to end the current iteration 
+        # in a for loop (or a while loop), and continues to the next iteration.
+        if flag == True:
+            continue
+
+        for j in range(0, i+1):
+            if arr[i] == arr[j]:
+                count += 1
+
+        print("{0}: {1}".format(arr[i], count))
+
+
+# Driver Code
+arr = [5, 8, 5, 7, 8, 10]
+n = len(arr)
+countFrequency(arr, n)
